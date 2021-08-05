758055c5e3c7fe642ca723eba7ff7f1eb65ef9c5
Isamu <isamu@Isamus-MacBook-Air.local>
2021-08-05 14:58:52 -0400

    nork package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2-dev
+
+* NOR/K Package
+
 ## 0.0.1-dev
 
 * TODO: Describe initial release.
--- lib/nork_ui.dart
+++ lib/nork_ui.dart
@@ -1,7 +1,10 @@
 library nork_ui;
 
+/// NOR/K a Flutter UI Kit
 class Nork {
+  /// Private Nork class
   const Nork._();
 
+  /// Returns [value] plus 1.
   static int addOne(int value) => value + 1;
 }
